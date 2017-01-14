f33d0c027633047e92da2dc7f0550ac444eb086e
kilimondjaro <kirill.babich@mail.ru>
2017-01-14 11:12:30 +0300

    structure fix

--- js/CouchsurfingApp.js
+++ js/CouchsurfingApp.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
+  StyleSheet
 } from 'react-native';
 
 class CouchsurfingApp extends Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Hello</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 export default CouchsurfingApp;
---
+++ js/redux/reducers/login.js
@@ -0,0 +1,7 @@
+const initialState = {
+  id: ''
+};
+
+export default function(state = initialState, action) {
+  return state;
+}
