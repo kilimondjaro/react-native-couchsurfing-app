981c93b27c18d0af4c88785a4c7e248e40a50b76
kilimondjaro <kirill.babich@mail.ru>
2017-02-04 23:58:26 +0300

    Implement calendar component with redux

--- js/redux/reducers/index.js
+++ js/redux/reducers/index.js
@@ -3,10 +3,12 @@ import login from './login';
 import navigation from './navigation';
 import location from './location';
 import signup from './signup';
+import calendar from './calendar';
 
 export default combineReducers({
   login,
   navigation,
   location,
-  signup
+  signup,
+  calendar
 });
