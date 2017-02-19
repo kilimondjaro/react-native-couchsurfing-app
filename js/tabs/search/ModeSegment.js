5785fb159f9cada57ddff9c052fe3e3505e87dd0
kilimondjaro <kirill.babich@mail.ru>
2017-02-19 19:30:28 +0300

    Implement ModeSegment

---
+++ js/tabs/search/ModeSegment.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native';
+
+type Props = {
+  icon: any;
+  text: string;
+  value: string;
+  onPress: (value: string) => void;
+  active: boolean;
+}
+
+export default function ModeSegment(props: Props) {
+  const {icon, text, value, onPress, active} = props;
+  const isActive = active === value;
+
+  return (
+    <TouchableOpacity
+      onPress={() => onPress(value)}
+      activeOpacity={0.8}
+      style={styles.container}
+    >
+      <Image source={props.icon} />
+      <Text style={isActive ? {color: 'blue'} : null}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
