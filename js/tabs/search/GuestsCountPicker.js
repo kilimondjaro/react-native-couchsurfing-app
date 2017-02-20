dfc42d45e0e785bf48e202e744060b2897df6df8
kilimondjaro <kirill.babich@mail.ru>
2017-02-20 17:25:39 +0300

    add extra values to GuestCountPicker

--- js/tabs/search/GuestsCountPicker.js
+++ js/tabs/search/GuestsCountPicker.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import {
   Text,
+  ScrollView,
   View,
   TouchableOpacity,
   StyleSheet
@@ -13,23 +14,32 @@ type Props = {
 };
 
 export default function GuestsCountPicker(props: Props) {
+  const count = 15;
+  var valuesArray = [];
+
+  for (let i = 1; i <= count; i++) {
+    valuesArray.push(i);
+  }
+
   return (
     <View style={styles.container}>
-      {[1, 2, 3, 4, 5, 6].map(val => {
-        const cellStyle = props.value === val ? {backgroundColor: '#2f81b7'} : null;
-        const cellTextStyle = props.value === val ? {color: 'white'} : {color: '#bfcad1'};
+      <ScrollView contentContainerStyle={styles.scrollView} horizontal>
+        {valuesArray.map(val => {
+          const cellStyle = props.value === val ? {backgroundColor: '#2f81b7'} : null;
+          const cellTextStyle = props.value === val ? {color: 'white'} : {color: '#bfcad1'};
 
-        return (
-          <TouchableOpacity
-            activeOpacity={0.8}
-            key={val}
-            style={[styles.cell, cellStyle]}
-            onPress={() => props.onPress(val)}
-          >
-            <Text style={[{fontSize: 18}, cellTextStyle]}>{val}</Text>
-          </TouchableOpacity>
-        );
-      })}
+          return (
+            <TouchableOpacity
+              activeOpacity={0.8}
+              key={val}
+              style={[styles.cell, cellStyle]}
+              onPress={() => props.onPress(val)}
+            >
+              <Text style={[{fontSize: 18}, cellTextStyle]}>{val}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
@@ -39,15 +49,18 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5,
     backgroundColor: 'white',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'row',
     borderBottomWidth: 1,
     borderColor: '#bfcad2'
+  },
+  scrollView: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
   cell: {
     height: 40,
     width: 40,
+    marginRight: 10,
+    marginLeft: 10,
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center'
