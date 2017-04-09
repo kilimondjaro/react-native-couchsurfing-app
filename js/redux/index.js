5c7dce4f236f497c03d51c6f5e5b7c95893eaa4c
kilimondjaro <kirill.babich@mail.ru>
2017-04-09 14:20:38 +0300

    add date transform to redux-persist

--- js/redux/index.js
+++ js/redux/index.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from 'redux';
+import traverse from 'traverse';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
-import { persistStore, autoRehydrate } from 'redux-persist';
+import { persistStore, autoRehydrate, createTransform } from 'redux-persist';
 import {AsyncStorage} from 'react-native';
 import combineReducers from './reducers/index';
 
@@ -15,11 +16,25 @@ const logger = createLogger({
 
 const createCustomStore = applyMiddleware(thunk, logger)(createStore);
 
+const dateTransform = createTransform(null, (outboundState) => {
+  return traverse(outboundState).map((val) => {
+    if (Date.parse(val)) {
+      return new Date(val);
+    }
+    return val;
+  });
+});
+
+
 function configureStore(onComplete: ?() => void) {
   // const store = createCustomStore(combineReducers);
   // setTimeout(onComplete, 100);
   const store = autoRehydrate()(createCustomStore)(combineReducers);
-  persistStore(store, { storage: AsyncStorage, blacklist: ['search', 'navigation', 'location', 'filter'] }, onComplete);
+  persistStore(store, {
+    storage: AsyncStorage,
+    blacklist: ['search', 'navigation', 'location', 'filter'],
+    transforms: [dateTransform]
+  }, onComplete);
   if (isDebuggingInChrome) {
     window.store = store;
   }
